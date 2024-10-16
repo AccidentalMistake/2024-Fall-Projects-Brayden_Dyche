7106efa144aea4144199008746b8392ac6e9ea5d
Accidental MIstake <18001870@students.sd308.org>
2024-10-15 19:25:32 -0500

    Added pigeon to shuffleboard

--- src/main/java/team/gif/robot/Robot.java
+++ src/main/java/team/gif/robot/Robot.java
@@ -56,6 +56,7 @@ public class Robot extends TimedRobot {
     talonSrxMotor.setDefaultCommand(new TalonJoystickControl());
     sparkMaxMotor = new SparkMaxMotor();
     robotContainer = new RobotContainer();
+    pigeon.addToShuffleboard("BAB", "Heading");
 
 
     ui = new UI();
