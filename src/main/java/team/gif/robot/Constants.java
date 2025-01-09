0fcf3eaa25acd00bf9c1568d4b9d61452d2e3ca2
Accidental MIstake <18001870@students.sd308.org>
2025-01-08 20:18:02 -0600

    Changed Ids

--- src/main/java/team/gif/robot/Constants.java
+++ src/main/java/team/gif/robot/Constants.java
@@ -18,16 +18,10 @@ import edu.wpi.first.math.util.Units;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static double CIM_MOTOR_PERCENT = 0.2;
-    public static double SPARKMAX_MOTOR_VOLTAGE = 2.5;
-    public static double SPARKMAX_MOTOR_RPM = 60;
+    public static double CIM_MOTOR_PERCENT = 0.1;
 
-    public final class NeoConstants{
-        public static double kP_NEO = 0.0004;
-        public static double FF_NEO = 0.000280;
 
 
     }
-}
 
 
--- src/main/java/team/gif/robot/RobotMap.java
+++ src/main/java/team/gif/robot/RobotMap.java
@@ -6,10 +6,10 @@ public abstract class RobotMap {
     public static final int AUX_CONTROLLER_ID = 1;
     public static final int TEST_CONTROLLER_ID = 2;
     public static final int PIGEON_ID = 5;
-    public static final int TALON_MOTOR_ID = 8;
+    public static final int TALON_MOTOR_ID = 2;
 
-    public static final int LEFT_FRONT_ID = 1;
-    public static final int LEFT_BACK_ID = 2;
-    public static final int RIGHT_FRONT_ID = 3;
-    public static final int RIGHT_BACK_ID = 4;
+    public static final int LEFT_FRONT_ID = 8;
+    public static final int LEFT_BACK_ID = 31;
+    public static final int RIGHT_FRONT_ID = 10;
+    public static final int RIGHT_BACK_ID = 1;
 }
